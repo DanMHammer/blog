c568179c6577738d81d380fe39ee1d9d2a43d25c
Dan Hammer <daniel.m.hammer@gmail.com>
2020-07-11 02:51:40 -0500

    made cbt link an absolute link

--- components/content/About.js
+++ components/content/About.js
@@ -8,12 +8,12 @@ const About = () => {
   return (
     <p>
       I'm Dan Hammer, a developer from Memphis, TN. I currently work for{" "}
-      <a href="crossbrowsertesting.com">CrossBrowserTesting</a>, a SmartBear
-      product. At CBT, I've created internal tools to improve the workflow for
-      our Support, DevOps, and Automation teams. I've also replaced the manual
-      process of installing software on our Windows VMs with an easy to use
-      automated process. Outside of work, I enjoy playing fetch and frisbee with
-      my Australian Shepherds (you can see pictures on the{" "}
+      <a href="https://crossbrowsertesting.com">CrossBrowserTesting</a>, a
+      SmartBear product. At CBT, I've created internal tools to improve the
+      workflow for our Support, DevOps, and Automation teams. I've also replaced
+      the manual process of installing software on our Windows VMs with an easy
+      to use automated process. Outside of work, I enjoy playing fetch and
+      frisbee with my Australian Shepherds (you can see pictures on the{" "}
       <Link href="/animals">
         <a>Animals Page</a>
       </Link>
