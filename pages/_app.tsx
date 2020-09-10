32f1aa7577ea72ede3097b9c21634b00386cfa39
Dan Hammer <daniel.m.hammer@gmail.com>
2020-09-10 13:54:16 -0500

    Beginning TypeScript conversion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeContextProvider } from "../components/context/ThemeContext";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeContextProvider>
       <Head>
